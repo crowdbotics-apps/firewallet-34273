fa4ad85bc2e28bd1d9fe4f4f204f239a921d63ed
Crowdbotics <team@crowdbotics.com>
2022-03-18 01:28:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "firewallet_34273",
+  url: "https://firewallet_34273.botics.co",
+  api: "https://firewallet_34273.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
